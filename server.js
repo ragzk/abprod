9da80371b5ef7d7e1436b860bb2a78ceeae51d22
Raghav <raghavatjanison@outlook.com>
2016-07-09 23:38:30 +1000

    chaned the watcher root path

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ watcher
       //console.log(event, path.extname(filePath));
       if (path.extname(filePath) == ".xml") {
           setTimeout(function () {
-              addFileToProcess(filePath);
+              addFileToProcess("./" + filePath);
               counter--;
           }, (counter++ * interval));
 
