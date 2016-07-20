211dbfdaf27113ebbb0c1905c0eee0eb953ed239
Raghav <raghavatjanison@outlook.com>
2016-07-21 03:49:03 +1000

    moved the defferd css Up and made changes for IP address

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var Promise = require('q');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'vash');
-
+app.set('trust proxy', 'loopback');
 
 
 // uncomment after placing your favicon in /public
